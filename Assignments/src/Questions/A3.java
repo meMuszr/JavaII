06ebfe6b0cd1ed7f0e4332fdbeac2fd4b4d2bd83
Mustafa Abdul-Kader <mabdulkader@FalconInsGroup.com>
2017-07-11 18:50:57 -0500

    pocket friends

--- Assignments/src/Questions/A3.java
+++ Assignments/src/Questions/A3.java
@@ -1,17 +1,85 @@
 package questions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 /**
  * Mustafa Abdul-Kader
  * CMPSC-203-01
  * Dr Mehta
  */
 public class A3 implements IRunner {
-
+	private Map<String,Integer> nameToInt = new TreeMap<>();
+	private Map<Integer,String> intToName = new HashMap<>();
+	private HashSet<String> uniqueOutput = new HashSet<>();
+	private int[][] effectiveFriends;
     public String getQuestionID() {
         return "A3";
     }
     public IRunner Run(String args) {
-
+    	try (Scanner scanner = new Scanner(new FileReader(args))) {
+            String[][] input = new String[scanner.nextInt()][];
+            scanner.nextLine();
+            effectiveFriends = new int[input.length][input.length];
+            for(int i = 0; i < input.length; i++) {
+            	input[i] = scanner.nextLine().split(" ");
+            	nameToInt.put(input[i][0],i);
+            	intToName.put(i,input[i][0]);
+            }
+            for(String[] row : input) 
+            	for(int i=1; i < row.length; i++) {
+            		effectiveFriends[nameToInt.get(row[0])][nameToInt.get(row[i])] = 1;
+            		effectiveFriends[nameToInt.get(row[i])][nameToInt.get(row[0])] = -1;
+            	}
+        	for(String friend : nameToInt.keySet()) findCycles(friend,"");
+            
+            return this;
+        } catch (InputMismatchException e) {
+            System.out.println("Error with input");
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't find file");
+        }
         return this;
     }
+    private String findCycles(String start, String output) {
+        	String[] amount = output.split(" ");
+    		if(amount.length == 3) return null;
+        	int[] strengths = effectiveFriends[nameToInt.get(start)];
+        	List<Integer> listOfBeatFriends = new ArrayList<>();
+        	for(int i=0; i < strengths.length; i++) {
+        		if(strengths[i] == 1) { 
+        			listOfBeatFriends.add(i);
+        		}
+        	}
+        	String curated = null;
+        	
+        	if(listOfBeatFriends.size() == 0) return output += "";
+    		if(amount.length == 2 && listOfBeatFriends.contains(nameToInt.get(amount[0]))) return output + start;
+    		
+        	for(Integer in : listOfBeatFriends) { 
+        		curated = findCycles(intToName.get(in), output + start + " ");
+        		if(curated != null) {
+        			String[] words = curated.split(" ");
+        			Arrays.sort(words);
+        			if(!uniqueOutput.contains(String.join("",words))) {
+            			uniqueOutput.add(String.join("",words));
+            			System.out.println(curated);
+            			break;
+        			}
+        			else return null;
+        		}
+
+        	}
+        	return curated;
+    }
+    	
 }
--- Assignments/src/Questions/N2.java
+++ Assignments/src/Questions/N2.java
@@ -1,16 +1,57 @@
 package questions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Mustafa Abdul-Kader
  * CMPSC-203-01
  * Dr Mehta
  */
 public class N2 implements IRunner {
+	private int[][] effectiveFriends;
     public String getQuestionID() {
         return "N2";
     }
     public IRunner Run(String args) {
-
+    	try (Scanner scanner = new Scanner(new FileReader(args))) {
+            int lines = scanner.nextInt();
+            scanner.nextLine();
+            while(lines > 0) {
+            	List<String> words = new ArrayList<>();
+            	for(String word : scanner.nextLine().split(" ")) words.add(word);
+            	int minIndex = findMinName(words);
+            	for(int j = 0; j < minIndex; j++) {
+            		String element = words.get(0);
+            		words.remove(0);
+            		words.add(element);
+            	}
+            	System.out.println(String.join(" ", words));
+            lines--;
+            }
+            
+            return this;
+        } catch (InputMismatchException e) {
+            System.out.println("Error with input");
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't find file");
+        }
         return this;
+    }
+    private int findMinName(List<String> names) {
+    	
+        String minVal = names.get(0); 
+        int minIdx = 0;
+        for(int i = 1; i < names.size(); i++) {
+        	if(names.get(i).compareTo(minVal) < 0) { 
+        			minVal = names.get(i);
+        			minIdx = i;
+        	}
+        }
+        return minIdx;
     }
 }
